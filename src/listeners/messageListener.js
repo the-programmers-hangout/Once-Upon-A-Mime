44b557fef6bd693d2313cdc00b8c1efdeede6eb8
Draxcodes <joelparkinson@live.co.uk>
2022-09-02 02:02:22 +0100

    Changes made and working thus far.
    - Include text content along with the uploaded file (Original request)
    - Include text content both before and after code blocks.
    - Upload code blocks greater than 20 lines to paste cord along with file.
    - Upload code blocks greater than 20 lines on their own. (Also extracts text before or after code block same as with a file upload)
    - Push a single message when multiple files are uploaded instead of many messages as it works right now.
    - Swap request for node-fetch as request seems to be deprecated.
    - Only pull the code block once when user uploads multiple files.
    - Handle multiple code blocks regardless of including a file or not.
    - Small code blocks that are sent alongside files or with a second larger code block are not uploaded but just sent back in a formatted way.
    - Code Cleaned Up (I think)

--- src/listeners/messageListener.js
+++ src/listeners/messageListener.js
@@ -2,8 +2,8 @@ import { Listener } from "@sapphire/framework";
 import { client } from "../index.js";
 import { checkMimeType } from "../services/mimeChecker.js";
 import { uploadFile } from "../services/fileUploader.js";
-import request from "request";
 import fs from "fs";
+import fetch from "node-fetch";
 
 export class MessageListener extends Listener {
   constructor(context, options) {
@@ -21,76 +21,176 @@ export class MessageListener extends Listener {
     const attachments = Array.from(message.attachments.values());
     const msgChannel = message.channel;
     const author = message.author.id;
+    const content = message.content;
+
+    // read configuration file
+    const configFile = fs.readFileSync("./config.json", "utf-8");
+    const config = JSON.parse(configFile);
+    const logChannel = config[message.guildId].logChannel;
+    const whitelistedMimes = config[message.guildId].whitelist;
+    const uploadableMimes = config[message.guildId].uploadableMimes;
+
+    // handle guild which has not been setup
+    if (!config[message.guildId]) {
+      message.channel.send(
+        "Bot not setup. Refer to: <https://github.com/DracTheDino/Mimey#readme>"
+      );
+      return;
+    }
 
     // helpful for multiple attachments
     var canDeleteMessage = false;
 
-    // loop through every attachment in message
-    for (const attachment of attachments) {
-      const fileName = attachment.name;
-      const fileUrl = attachment.url;
+    const codeBlocks = content.match(/```[a-z0-9\s]{0,10}\n([\s\S]*?)```/gm);
 
-      // check for mime type
-      if (!(await checkMimeType(fileUrl))) {
-        if (!attachment.contentType) {
-          canDeleteMessage = true;
-        } else {
-          var mimeType = attachment.contentType.substring(
-            0,
-            attachment.contentType.indexOf(";")
-          );
-        }
-      } else {
-        var mimeType = (await checkMimeType(fileUrl)).mime;
-      }
+    // loop through every attachment in message if there are any
+    if (attachments.length >= 1) {
+      let finalReplyMessage = [
+        `Hey <@${author}>, your file(s) have been uploaded to Pastecord.`,
+      ];
+      // Check FIRST, before we loop through any files if message includes any code blocks at all.
+      if (codeBlocks !== null) {
+        let codeblocksToBeRemoved = [];
+        for (const [index, block] of codeBlocks.entries()) {
+          let codeArray = block.split("\n");
+          codeblocksToBeRemoved.push(block);
 
-      // read configuration file
-      const configFile = fs.readFileSync("./config.json", "utf-8");
-      const config = JSON.parse(configFile);
+          if (codeArray.length >= 20) {
+            codeArray.splice(0, 1);
+            const formattedCode = codeArray.join("\n");
+            const url = await uploadFile(formattedCode);
+            //const url = "Fake Upload"; //For dev purposes
+            finalReplyMessage.push(`**Code snippet [${index + 1}]:** ${url}`);
 
-      // handle guild which has not been setup
-      if (!config[message.guildId]) {
-        message.channel.send(
-          "Bot not setup. Refer to: <https://github.com/DracTheDino/Mimey#readme>"
-        );
-        return;
-      }
-
-      const logChannel = config[message.guildId].logChannel;
-      const whitelistedMimes = config[message.guildId].whitelist;
-      const uploadableMimes = config[message.guildId].uploadableMimes;
-
-      // check if mimeType is on the uploadableMimes list
-      if (uploadableMimes.includes(mimeType)) {
-        canDeleteMessage = true;
-
-        request.get(fileUrl, function (error, response, body) {
-          if (error) throw error;
-
-          uploadFile(body).then((url) => {
-            msgChannel.send(
-              `Hey <@${author}>, your file \`${fileName}\` has been uploaded to Pastecord: ${url}`
-            );
             client.channels.cache
               .get(logChannel)
               .send(
-                `Deleted file \`${fileName}\` of type \`${mimeType}\` from user <@${author}> in <#${msgChannel.id}>. File uploaded to Pastecord: ${url}`
+                `Deleted large code block from user <@${author}> in <#${msgChannel.id}>. Uploaded to Pastecord: ${url}`
               );
-          });
-        });
-      }
-      // if that fails, check if its whitelisted
-      else if (!whitelistedMimes.includes(mimeType)) {
-        canDeleteMessage = true;
+          } else {
+            const formattedCode = codeArray.join("\n");
+            finalReplyMessage.push(
+              `**Code snippet [${index + 1}]:** ${formattedCode}`
+            );
+          }
+        }
 
-        msgChannel.send(
-          `Hey <@${author}>, please don't upload \`${mimeType}\` files on this server.`
-        );
-        client.channels.cache
-          .get(logChannel)
-          .send(
-            `Deleted file \`${fileName}\` of type \`${mimeType}\` from user <@${author}> in <#${msgChannel.id}>.`
+        //if content before or after code block
+        let finalMessageContent = content;
+        for (const snippet of codeblocksToBeRemoved) {
+          finalMessageContent = finalMessageContent.replace(snippet, "");
+        }
+
+        if (finalMessageContent.length > 1) {
+          finalReplyMessage.push(
+            `**Message text content:** \`\`\`\n${finalMessageContent}\`\`\``
           );
+        }
+      }
+
+      for (const attachment of attachments) {
+        const fileName = attachment.name;
+        const fileUrl = attachment.url;
+
+        // check for mime type
+        if (!(await checkMimeType(fileUrl))) {
+          if (!attachment.contentType) {
+            canDeleteMessage = true;
+          } else {
+            var mimeType = attachment.contentType.substring(
+              0,
+              attachment.contentType.indexOf(";")
+            );
+          }
+        } else {
+          var mimeType = (await checkMimeType(fileUrl)).mime;
+        }
+
+        // check if mimeType is on the uploadableMimes list
+        if (uploadableMimes.includes(mimeType)) {
+          canDeleteMessage = true;
+
+          const response = await fetch(fileUrl);
+          const body = await response.text();
+          const url = await uploadFile(body);
+          //const url = `${fileName}.uploaded`; //For dev purposes
+          finalReplyMessage.push(`**${fileName}:** ${url}`);
+
+          client.channels.cache
+            .get(logChannel)
+            .send(
+              `Deleted file \`${fileName}\` of type \`${mimeType}\` from user <@${author}> in <#${msgChannel.id}>. Pastecord: ${url}`
+            );
+        }
+        // if that fails, check if its whitelisted
+        else if (!whitelistedMimes.includes(mimeType)) {
+          canDeleteMessage = true;
+
+          msgChannel.send(
+            `Hey <@${author}>, please don't upload \`${mimeType}\` files on this server.`
+          );
+
+          client.channels.cache
+            .get(logChannel)
+            .send(
+              `Deleted file \`${fileName}\` of type \`${mimeType}\` from user <@${author}> in <#${msgChannel.id}>.`
+            );
+        }
+      }
+
+      const fullReplyMessage = finalReplyMessage.join("\n");
+      msgChannel.send(fullReplyMessage);
+    }
+
+    //Upload Code Snippet is the block is larger than 20 lines.
+    if (attachments.length < 1 && codeBlocks !== null) {
+      const logChannel = config[message.guildId].logChannel;
+      let shouldSendMessage = false;
+      let finalReplyMessage = [
+        `Hey <@${author}>, your file(s) have been uploaded to Pastecord.`,
+      ];
+
+      let codeblocksToBeRemoved = [];
+      for (const [index, block] of codeBlocks.entries()) {
+        let codeArray = block.split("\n");
+        codeblocksToBeRemoved.push(block);
+
+        if (codeArray.length >= 20) {
+          shouldSendMessage = true;
+          codeArray.splice(0, 1);
+          const formattedCode = codeArray.join("\n");
+          const url = await uploadFile(formattedCode);
+          //const url = "Fake Upload";  //For Dev purposes
+          finalReplyMessage.push(`**Code snippet [${index + 1}]:** ${url}`);
+
+          client.channels.cache
+            .get(logChannel)
+            .send(
+              `Deleted large code block from user <@${author}> in <#${msgChannel.id}>. Uploaded to Pastecord: ${url}`
+            );
+          canDeleteMessage = true;
+        } else {
+          finalReplyMessage.push(`**Code snippet [${index + 1}]:**\n${block}`);
+        }
+      }
+
+      //grab any other content than code blocks
+      let finalMessageContent = content;
+      for (const snippet of codeblocksToBeRemoved) {
+        finalMessageContent = finalMessageContent.replace(snippet, "");
+      }
+
+      //Check there is anything other than code blocks to send back.
+      if (finalMessageContent.length > 1) {
+        finalReplyMessage.push(
+          `**Message text content:** \`\`\`\n${finalMessageContent}\`\`\`\n`
+        );
+      }
+
+      //If anything has been uploaded, this should be true and will reply.
+      if (shouldSendMessage) {
+        const fullReplyMessage = finalReplyMessage.join("\n");
+        msgChannel.send(fullReplyMessage);
       }
     }
 
